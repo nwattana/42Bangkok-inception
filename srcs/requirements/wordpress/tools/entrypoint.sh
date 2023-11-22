4b9771e0a7247f793f7c8da7e646d982f0114b8c
nwattana <nwattana@Debby.myguest.virtualbox.org>
2023-11-22 19:25:49 +0700

    dump db

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -17,7 +17,8 @@ else
 	sed -i "s/localhost/$WORDPRESS_DB_HOST/g" wp-config.php
 	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" wp-config.php
 
-	wp user create  --allow-root $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_EMAIL --role=administrator --user_pass=$WORDPRESS_ADMIN_PASS --display_name=admin
+	wp core install --allow-root --url=$DOMAIN_NAME --title=$LOGIN_ID --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email
+	# wp user create  --allow-root $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_EMAIL --role=administrator --user_pass=$WORDPRESS_ADMIN_PASS --display_name=admin
 	wp user create  --allow-root $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor --user_pass=$WORDPRESS_USER_PASS --display_name=user1
 fi
 
